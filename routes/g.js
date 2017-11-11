e54a38ca839922e0cabdf0a4c0b7f82d2c8ca203
喆 <Lz0o_o0@outlook.com>
2017-11-11 14:30:25 +0900

    LJS Second commit .......ejs

--- routes/g.js
+++ routes/g.js
@@ -2,7 +2,38 @@ var express = require('express');
 var path = require('path');
 var g = express.Router();
 
+
+
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+db.on('error', (err) => {console.log(`mongoose ^^ ${err}`)});
+db.on('open', () => {console.log("mongoose 数据库开启...")});
+mongoose.connect("mongodb://192.168.0.135/stus", {useMongoClient: true});
+var Schema = mongoose.Schema;
+// var touZiSchema = new Schema({
+//   title: String,
+//   incomeNum: Number,
+//   income: String,
+//   year: Number,
+//   yearType: String,
+//   money: String,
+//   star: Number,
+//   program: Number,
+//   mayMoney: String,
+//   src: String
+// });
+
+// var WoYaoTouZi = mongoose.model("Three", touZiSchema);
+// new addData({});
 var options = {root: __dirname + "/../public"};
+
+
+
+
+
+
+
 
 /* GET home page. */
 g.get('/logon', function(req, res, next) {
--- routes/l.js
+++ routes/l.js
@@ -1,18 +1,33 @@
 var express = require('express');
+
 var l = express.Router();
 
 
 
 /* GET home page. */
-l.get('/', function(req, res, next) {
-  res.render('q_xw');
+
+
+l.get('/about/gltd',function(req,res){
+  res.render('l-gy-gltd')
 });
-
-
-
-
-
-
+l.get('/about',function(req,res){
+  res.render('l-gy')
+});
+l.get('/about/dstd',function(req,res){
+  res.render('l-gy-dstd')
+});
+l.get('/about/fzlc',function(req,res){
+  res.render('l-gy-fzlc')
+});
+l.get('/about/hzhb',function(req,res){
+  res.render('l-gy-hzhb')
+});
+l.get('/about/yqlj',function(req,res){
+  res.render('l-gy-yqlj')
+});
+l.get('/about/lxwm',function(req,res){
+  res.render('l-gy-lxwm')
+});
 
 
 
